12b9c697a720c84875ac6f6be0a3e7eefd306f35
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-03-30 23:53:43 +0400

    Ok

---
+++ src/main.py
@@ -0,0 +1,6 @@
+import requests
+import argparse
+
+findargs = argparse.ArgumentParser("https://www.youtube.com/watch?v=dVVZaZ8yO6o")
+findargs.add_argument("link", help="Link to video, could be tiktok or youtube")
+
