1f9ecf1b72542dca7c2e4626e9bd397dbc3b53f8
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-04-06 01:57:55 +0400

    Switch from powershell scripts to python for tests

--- src/arghandler.py
+++ src/arghandler.py
@@ -4,6 +4,3 @@ parser = argparse.ArgumentParser("[LINK]")
 parser.add_argument("link", help="Link to video (Youtube, Tiktok)")
 args = parser.parse_args()
 
-# debug
-# this makes it print twice
-#print(args.link)
--- tests/test.py
+++ tests/test.py
@@ -8,8 +8,10 @@ youtube = os.path.isfile("videos/youtube/video.mp4")
 tiktok = os.path.isfile("videos/tiktok/video.mp4")
 
 # Check if regular works
+print(colorama.Fore.CYAN + "Testing No args")
 os.system("python src/main.py")
 print(colorama.Fore.BLUE + "No args test complete!")
 
 # Youtube Download test
-# TODO:: Finish this
+os.system("python src/main.py https://www.youtube.com/watch?v=aqz-KE-bpKQ")
+print("Testing Youtube Download")
